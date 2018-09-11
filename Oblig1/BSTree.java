ac2efebab6068108c6389abdd6128abd13032f34
Carl <carl@live.no>
2018-09-11 15:23:06 +0200

    SortedINt done

--- Oblig1/BSTree.java
+++ Oblig1/BSTree.java
@@ -19,23 +19,24 @@ public class BSTree implements BSTOper {
         }
     }
 
-    public void printTree(){
+    public void printTree() {
         printTree(root);
     }
-    public void printTree(Node node){
+
+    public void printTree(Node node) {
         System.out.println(node.value);
-        if(node.left != null){
+        if (node.left != null) {
             System.out.println(node.value + " er faren til " + node.left.value);
             System.out.println();
             printTree(node.left);
-        }else{
+        } else {
             System.out.println("lovnode");
         }
-        if(node.right != null){
+        if (node.right != null) {
             System.out.println(node.value + " er faren til " + node.right.value);
             System.out.println();
             printTree(node.right);
-        }else{
+        } else {
             System.out.println("lovnode");
             System.out.println();
         }
@@ -50,39 +51,64 @@ public class BSTree implements BSTOper {
         }
     }
 
-    public void add(int value, Node currNode){
-        if (value == currNode.value){
+    public void add(int value, Node curNode) {
+        if (value == curNode.value) {
             System.out.println("Duplicate node found, terminated node insertion");
             return;
-        }else if(value < currNode.value){
-            if(currNode.left == null){
-                currNode.left = new Node(value);
-                System.out.println("Node added left with value " + value + " with parent node " + currNode.value);
+        } else if (value < curNode.value) {
+            if (curNode.left == null) {
+                curNode.left = new Node(value);
+                System.out.println("Node added left with value " + value + " with parent node " + curNode.value);
                 return;
-            }else{
-                add(value, currNode.left);
-                System.out.println("Moved to the left from " + currNode.value + " to " + currNode.left.value);
+            } else {
+                add(value, curNode.left);
+                System.out.println("Moved to the left from " + curNode.value + " to " + curNode.left.value);
             }
-        }else if(value > currNode.value) {
-            if(currNode.right == null){
-                currNode.right = new Node(value);
-                System.out.println("Node added left with value " + value + " with parent node " + currNode.value);
+        } else if (value > curNode.value) {
+            if (curNode.right == null) {
+                curNode.right = new Node(value);
+                System.out.println("Node added left with value " + value + " with parent node " + curNode.value);
                 return;
-            }else{
-                System.out.println("Moved to the left from " + currNode.value + " to " + currNode.right.value);
-                add(value, currNode.right);
+            } else {
+                System.out.println("Moved to the left from " + curNode.value + " to " + curNode.right.value);
+                add(value, curNode.right);
             }
         }
     }
-
-    public boolean remove(int value) {
-
-        if (root == null) {
-            System.out.println("Tree is empty, nothing to remove");
+    public boolean remove(int value){
+        if(existsInTree(value)){
+            return remove(value, root);
+        }else{
             return false;
         }
-        if(existsInTree(value)){
+    }
+
+    public boolean remove(int value, Node curNode) {
+        curNode = find(value);
+        if(curNode.left == null && curNode.right == null){
+            parent = findParent(curNode);
+            if(parent.left.value != null && parent.left.value == curNode.left.value){
+                parent.left = null;
+                return true;
+            }else{
+                parent.right = null;
+                return true;
+            }
         }
+
+        if(curNode.left != null && curNode.right != null){
+            parent = findParent(curNode);
+            if(parent.left.value == curNode.left.value){
+                
+            }else{
+                parent.right = null;
+                return true;
+            }
+        }
+
+
+
+
         return false;
     }
 
@@ -99,32 +125,22 @@ public class BSTree implements BSTOper {
     }
 
     public boolean existsInTree(int value) {
-        return existsInTree(value, root);
-    }
-
-    public boolean existsInTree(int value, Node currNode) {
-        if (currNode == null) {
+        if (root == null) {
             System.out.println("Value does not exist in tree, tree doesnt have any nodes");
             return false;
         }
-        if(value == currNode.value){
-            System.out.println("Value " + value + " does exist in tree");
+        return existsInTree(value, root);
+    }
+
+    public boolean existsInTree(int value, Node curNode) {
+        if (curNode.value == value) {
             return true;
         }
-        if(value < currNode.value){
-            if(currNode.left == null){
-                System.out.println(value + "Does not exist in tree");
-                return false;
-            }else{
-                existsInTree(value, currNode.left);
-            }
-        }else{
-            if(currNode.right == null){
-                System.out.println(value + "Does not exist in tree");
-                return false;
-            }else{
-                existsInTree(value, currNode.right);
-            }
+        if (curNode.left != null && existsInTree(value, curNode.left)) {
+            return true;
+        }
+        if (curNode.right != null && existsInTree(value, curNode.right)) {
+            return true;
         }
         return false;
     }
@@ -137,15 +153,15 @@ public class BSTree implements BSTOper {
         return findNearestSmallerThan(value, root, nearest);
     }
 
-    public int findNearestSmallerThan(int value, Node currNode, int nearest) {
-        if (currNode == null) {
+    public int findNearestSmallerThan(int value, Node curNode, int nearest) {
+        if (curNode == null) {
             return nearest;
         }
-        if (value > currNode.value && currNode.value > nearest) {
-            System.out.println("Nearest updated to " + currNode.value);
-            return (findNearestSmallerThan(value, currNode, currNode.value));
+        if (value > curNode.value && curNode.value > nearest) {
+            System.out.println("Nearest updated to " + curNode.value);
+            return (findNearestSmallerThan(value, curNode, curNode.value));
         } else {
-            return (findNearestSmallerThan(value, currNode, nearest));
+            return (findNearestSmallerThan(value, curNode, nearest));
         }
     }
 
@@ -161,43 +177,70 @@ public class BSTree implements BSTOper {
 
     public int[] sortedArray() {
         int[] sortedNodes = new int[size()];
-        int index = 0;
-        return sortedArray(root, sortedNodes, index);
+        int[] index = { 0 };
+        sortedArray(root, sortedNodes, index);
+        return sortedNodes;
     }
 
-    public int[] sortedArray(Node currNode, int[] sortedNodes) {
-        if (currNode.left != null) {
-            sortedArray(currNode.left, sortedNodes);
+    public void sortedArray(Node curNode, int[] sortedNodes, int[] index) {
+        if (curNode == null) {
+            return;
         }
-
-        if (currNode.right != null) {
-            sortedArray(currNode.left, sortedNodes);
-        }
-        return sortedNodes;
+        sortedArray(curNode.left, sortedNodes, index);
+        sortedNodes[index[0]] = curNode.value;
+        index[0]++;
+        sortedArray(curNode.right, sortedNodes, index);
     }
 
     public int[] findInRange(int low, int high) {
         int[] tree = sortedArray();
         int length = high - low;
         int[] inRange = new int[length];
-        for(int i = 0; i < tree.length; i++){
-            if(high > tree[i] && tree[i] > low){
+        for (int i = 0; i < tree.length; i++) {
+            if (high >= tree[i] && tree[i] >= low) {
                 inRange[i] = tree[i];
             }
-        }      
+        }
         return inRange;
     }
 
     // konstruktører til BSTree
-    private Node findParent(Node n) {
+    private Node findParent(Node lookFor) {
+        return findParent(lookFor, root, root);
+    }
+
+    private Node findParent(Node lookFor, Node currNode, Node parent) {
+        if (lookFor.value == root.value) {
+            return null;
+        }
+        if (currNode.left.value == lookFor.value || currNode.right.value == lookFor.value) {
+            return currNode;
+        } else if (lookFor.value < currNode.value) {
+            findParent(lookFor, currNode.left, currNode);
+        } else if (lookFor.value > currNode.value) {
+            findParent(lookFor, currNode.right, currNode);
+        }
         return null;
     }
 
     private Node findGrandparent(Node n) {
-        return null;
+        return findParent(findParent(n));
     }
 
     private Node find(int value) {
+        return find(value, root);
+    }
+
+    public Node find(int value, Node curNode) {
+        if (curNode.value == value) {
+            return curNode;
+        }
+        if (value < curNode.value && curNode.left != null) {
+            find(value, curNode.left);
+        }
+        if (value > curNode.value && curNode.right != null) {
+            find(value, curNode.right);
+        }
         return null;
     }
 
